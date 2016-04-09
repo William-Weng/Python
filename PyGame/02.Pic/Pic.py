0c02a55e1e22ec1d2f92254d749c0a5449493897
William-Weng <linuxice0609@gmail.com>
2016-04-09 19:45:14 +0800

    放入圖片

---
+++ PyGame/02.Pic/Pic.py
@@ -0,0 +1,25 @@
+import pygame
+from sys import exit
+from pygame.locals import *
+
+pygame.init()
+
+screen = pygame.display.set_mode((640, 453), 0, 32)  # 設定畫面大小
+pygame.display.set_caption('Python-Pygame')  # 設定標題
+
+bgPic = pygame.image.load('./images/python.png').convert()  # 載入背景圖
+mousePic = pygame.image.load('./images/mouse.png').convert_alpha()  # 載入圖片（含透明度）
+
+while True:
+    for event in pygame.event.get():
+        if(event.type == pygame.QUIT):
+            exit()
+
+    x, y = pygame.mouse.get_pos()  # 取得滑標的位置
+    x -= mousePic.get_width() / 2  # 取得中心點，讓鼠標在圖形的正中央
+    y -= mousePic.get_height() / 2
+
+    screen.blit(bgPic, (0, 0))  # 把圖畫上去
+    screen.blit(mousePic, (x, y))
+
+    pygame.display.update()  # 更新畫面
