2633f2f48ae082e645f93f193a4c5f50ce7d3981
William-Weng <linuxice0609@gmail.com>
2016-04-12 19:30:43 +0800

    產生出RGB所有的顏色

---
+++ PyGame/02.Pic/Color.py
@@ -0,0 +1,17 @@
+#-*- coding: UTF-8 -*-
+
+import pygame
+
+pygame.init()
+
+allColors = pygame.Surface((4096, 4096), depth=24)  # 4096 * 4096 * 24bits
+
+for r in range(256):  # rgb ==> (0,0,0) ~ (255,255,255)
+    print(r + 1, "out of 256")
+    x, y = (r & 0b1111) * 256, (r >> 0b100) * 256
+
+    for g in range(256):
+        for b in range(256):
+            allColors.set_at((x + g, y + b), (r, g, b))
+
+pygame.image.save(allColors, "allColors.bmp")
